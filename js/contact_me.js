fc73b0bfbff3fe154951b78055bb4e4344130880
Ayumi Ono <ayumiono.1223@gmail.com>
2019-05-24 18:13:49 -0400

    fix(contact): added label tags in form

--- js/contact_me.js
+++ js/contact_me.js
@@ -25,7 +25,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append('</button>')
           $('#success > .alert-success')
-            .append($('<strong>').text('Thank you, ' + firstName + '!<br>Your message has been sent.'))
+            .append($('<strong>').text('Thank you, ' + firstName + '! Your message has been sent.'))
           $('#success > .alert-success')
             .append('</div>')
           //clear all fields
