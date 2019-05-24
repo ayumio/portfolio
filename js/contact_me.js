8ca9b41b6ac0a30410df196b932522007fb80645
Ayumi Ono <ayumiono.1223@gmail.com>
2019-05-24 17:15:23 -0400

    fix(contact): added a validation

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,21 +1,3 @@
-// $('#contactForm input, #contactForm textarea').submit(function(event) {
-//   event.preventDefault()
-
-//   var $form = $(this)
-//   $.post($form.attr('action'), $form.serialize()).then(function() {
-//     $('#success').html("<div class='alert alert-success'>")
-//     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-//       .append('</button>')
-//     $('#success > .alert-success')
-//       .append('<strong>Thank you! Your message has been sent.</strong>')
-//     $('#success > .alert-success')
-//       .append('</div>')
-//     //clear all fields
-//     $('#contactForm').trigger('reset');
-//   })
-// })
-
-
 $(function() {
 
   $('#contactForm input,#contactForm textarea').jqBootstrapValidation({
@@ -27,6 +9,8 @@ $(function() {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $('input#name').val();
+      var email = $('input#email').val();
+      var message = $('input#message').val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -34,24 +18,26 @@ $(function() {
       }
       var $this = $('#sendMessageButton')
       $this.prop('disabled', true)
-      var $form = $(this)
-      $.post($form.attr('action'), $form.serialize()).then(function(){
-        $('#success').html("<div class='alert alert-success'>")
-        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-          .append('</button>')
-        $('#success > .alert-success')
-          .append('<strong>Thank you!<br>Your message has been sent.</strong>')
-        $('#success > .alert-success')
-          .append('</div>')
-        //clear all fields
-        $('#contactForm').trigger('reset')
-      })
-      .then(setTimeout(function() {
-        $this.prop('disabled', false) // Re-enable submit button when AJAX call is complete
-      }), 1000)
-      .then(function() {
+      var $form = $('#contactForm')
+      if (name && email && message) {
+        $.post($form.attr('action'), $form.serialize()).then(function(){
+          $('#success').html("<div class='alert alert-success'>")
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append('</button>')
+          $('#success > .alert-success')
+            .append('<strong>Thank you!<br>Your message has been sent.</strong>')
+          $('#success > .alert-success')
+            .append('</div>')
+          //clear all fields
+          $('#contactForm').trigger('reset')
+        })
+        .then(setTimeout(function() {
+          $this.prop('disabled', false) // Re-enable submit button when AJAX call is complete
+        }), 1000)
+        .then(function() {
           return $(this).is(':visible')
-      })
+        })
+      }
     })
   })
   $('a[data-toggle="tab"]').click(function(e) {
