58e0d54eac0b7e20b12da2ae60dbd81779c130b1
Ayumi Ono <ayumiono.1223@gmail.com>
2019-05-24 17:39:06 -0400

    fix(contact): fixed validation

--- js/contact_me.js
+++ js/contact_me.js
@@ -16,16 +16,16 @@ $(function() {
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ')
       }
-      var $this = $('#sendMessageButton')
-      $this.prop('disabled', true)
-      var $form = $('#contactForm')
       if (name && email && message) {
+        var $this = $('#sendMessageButton')
+        $this.prop('disabled', true)
+        var $form = $('#contactForm')
         $.post($form.attr('action'), $form.serialize()).then(function(){
           $('#success').html("<div class='alert alert-success'>")
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append('</button>')
           $('#success > .alert-success')
-            .append('<strong>Thank you!<br>Your message has been sent.</strong>')
+            .append($('<strong>').text('Thank you, ' + firstName + '!<br>Your message has been sent.'))
           $('#success > .alert-success')
             .append('</div>')
           //clear all fields
