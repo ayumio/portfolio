216cd19c4efd64659f023c711fa811672bec8f20
Ayumi Ono <ayumiono.1223@gmail.com>
2019-05-24 18:00:15 -0400

    fix(contact): fixed a syntax

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,13 +10,13 @@ $(function() {
       // get values from FORM
       var name = $('input#name').val()
       var email = $('input#email').val()
-      var message = $('input#message').val()
+      var message = $('textarea#message').val()
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ')
       }
-      // if (name && email && message) {
+      if (name && email && message) {
         var $this = $('#sendMessageButton')
         var $form = $(this)
         $this.prop('disabled', true)
@@ -37,7 +37,7 @@ $(function() {
         .then(function() {
           return $(this).is(':visible')
         })
-      // }
+      }
     })
   })
   $('a[data-toggle="tab"]').click(function(e) {
